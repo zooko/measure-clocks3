c582b564e7423263aed67b72a0993a45e1d11654
Zsomething <nothanks@ontheemail.com>
2025-12-07 14:49:49 -0700

    add results from my macbook pro m4 max

--- run-measurement.sh
+++ run-measurement.sh
@@ -2,4 +2,6 @@
 
 OS=`uname -s`
 PROC=`./detect-cpu-details.sh | grep "CPU Model" | cut -d' ' -f3-`
-LOGF=./results/$OS-$PROC-`date -u "+%Y-%m-%d_%H-%M-%S"`.txt; echo "./target/release/measure-clocks $*" > $LOGF ; ./target/release/measure-clocks $* 2>&1 | tee -a $LOGF
+LOGD=./results
+mkdir -p $LOGD
+LOGF=$LOGD/$OS-$PROC-`date -u "+%Y-%m-%d_%H-%M-%S"`.txt; echo "./target/release/measure-clocks $*" > $LOGF ; ./target/release/measure-clocks $* 2>&1 | tee -a $LOGF
